8ce6fc777f0a0c56f9b92294b177bd9e65c90bca
TataProgrammer <73477872+TataProgrammer@users.noreply.github.com>
2020-10-29 11:22:23 +0300

    third

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+// console.log(1);
+// console.log(true);
+
+// var num = +prompt('введите год рождения');
+// var year = 2020;
+// console.log(num);
+// alert(year - num);
+
+
+var name = prompt('введите своё имя'),
+ age = prompt('сколько вам лет?'),
+ answer1 = prompt('решите пример 6 + 3 = ?'),
+ answer2 = prompt('решите пример 20 - 5 = ?'),
+ answer3 = prompt('решите пример 15 * 2 = ?'),
+ answer4 = prompt('решите пример 8 / 4 = ?'),
+ answer5 = prompt('решите пример 10 % 3 = ?');
+
+console.log(' Меня зовут ' + name);
+console.log(' Мне ' + age + ' лет ');
+console.log(' 6 + 3 = 9 ваш ответ ' + answer1);
+console.log(' 20 - 5 = 15 ваш ответ ' + answer2);
+console.log(' 15 * 2 = 30 ваш ответ ' +  answer3);
+console.log(' 8 / 4 = 2 ваш ответ ' + answer4);
+console.log(' 10 % 3 = 1 ваш ответ ' + answer5);
